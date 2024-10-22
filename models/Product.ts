9ed4e150456a2abe763933396295cc296d1ef541
keekmx1 <kitthisack180897@gmail.com>
2024-10-22 15:54:33 +0700

    Create product model

--- models/Product.ts
+++ models/Product.ts
@@ -1,12 +1,36 @@
 // Define and interface for Product
-export interface Product {
-    name: string
-    description: string
-    barcode: string
-    image: string
-    stock: number
-    price: number
-    category_id: number
-    user_id: number
-    status_id: number
+interface Product {
+  name: string;
+  description: string;
+  barcode: string;
+  image: string;
+  stock: number;
+  price: number;
+  category_id: number;
+  user_id: number;
+  status_id: number;
+}
+
+class Product {
+  name: string;
+  description: string;
+  barcode: string;
+  image: string;
+  stock: number;
+  price: number;
+  category_id: number;
+  user_id: number;
+  status_id: number;
+
+  constructor(product: Product) {
+    this.name = product.name;
+    this.description = product.description;
+    this.barcode = product.barcode;
+    this.image = product.image;
+    this.stock = product.stock;
+    this.price = product.price;
+    this.category_id = product.category_id;
+    this.user_id = product.user_id;
+    this.status_id = product.status_id;
+  }
 }
