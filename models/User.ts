4d872ec3c5324b7a58e72bc72a1e632a3db8e621
keekmx1 <kitthisack180897@gmail.com>
2024-10-22 14:11:02 +0700

    create model user

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+// ຂໍ້ມູນ User ທີ່ຈະສ້າງ
+// id, firstname, lastname, email, password, avrtar, timestamp
+
+interface User{
+    id: number
+    firstname: string
+    lastname: string
+    email: string
+    password: string
+    avatart: string
+    timestamp: number
+}
