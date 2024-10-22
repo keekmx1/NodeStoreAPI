588b9eec602fa1db531b5573141d63336474907b
keekmx1 <kitthisack180897@gmail.com>
2024-10-22 11:32:48 +0700

    install mysql2

--- server.ts
+++ server.ts
@@ -12,6 +12,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, this is TypeScript nodejs project");
 });
 
+// Test Connection Database MySQL
+app.get("/testdb", (req: Request, res: Response) => {
+  res.send("");
+});
+
 // Start Server on port 3000
 const port: string | number = process.env.PORT || 3000;
 app.listen(port, () => {
