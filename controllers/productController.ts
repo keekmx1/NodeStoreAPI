b242d443a8158cad898f646b3aff05239b182d6b
keekmx1 <kitthisack180897@gmail.com>
2024-10-23 14:19:53 +0700

    add image funtion

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,5 +1,9 @@
 import e, { Request, Response } from "express";
 import connection from "../utils/db";
+import multer from "multer";
+import multerConfig from "../utils/multer_config";
+
+const upload = multer(multerConfig.config).single(multerConfig.keyUpload);
 
 // ----------------------------
 // Get All Products
@@ -63,55 +67,67 @@ function getProductById(req: Request, res: Response) {
 // ----------------------------
 
 function createProduct(req: Request, res: Response) {
-  try {
-    const { name, price } = req.body;
-    connection.execute(
-      "INSERT INTO products (name, description, barcode, image, stock, price, category_id, user_id, status_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-      [
-        req.body.name,
-        req.body.description,
-        req.body.barcode,
-        req.body.image,
-        req.body.stock,
-        req.body.price,
-        req.body.category_id,
-        req.body.user_id,
-        req.body.status_id,
-      ],
-      function (error: any, results: any) {
-        if (error) {
-          res.json({
-            status: "error",
-            message: error,
-          });
-          return;
-        } else {
-          res.json({
-            status: "ok",
-            message: "Product created successfully",
-            product: {
-              id: results.insertId,
-              name: req.body.name,
-              description: req.body.description,
-              barcode: req.body.barcode,
-              image: req.body.image,
-              stock: req.body.stock,
-              price: req.body.price,
-              category_id: req.body.category_id,
-              user_id: req.body.user_id,
-              status_id: req.body.status_id,
-            },
-          });
-        }
+  upload(req, res, async (error) => {
+    if (error instanceof multer.MulterError) {
+      res.json({
+        status: "error",
+        message: error,
+      });
+      return;
+    } else {
+      const image = req.file ? req.file.filename : null;
+
+      try {
+        // get data from request
+        connection.execute(
+          "INSERT INTO products (name, description, barcode, image, stock, price, category_id, user_id, status_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [
+            req.body.name,
+            req.body.description,
+            req.body.barcode,
+            image,
+            req.body.stock,
+            req.body.price,
+            req.body.category_id,
+            req.body.user_id,
+            req.body.status_id,
+          ],
+          function (error: any, results: any) {
+            if (error) {
+              res.json({
+                status: "error",
+                message: error,
+              });
+              return;
+            } else {
+              res.json({
+                status: "ok",
+                message: "Product created successfully",
+                product: {
+                  id: results.insertId,
+                  name: req.body.name,
+                  description: req.body.description,
+                  barcode: req.body.barcode,
+                  image: req.body.image,
+                  stock: req.body.stock,
+                  price: req.body.price,
+                  category_id: req.body.category_id,
+                  user_id: req.body.user_id,
+                  status_id: req.body.status_id,
+                },
+              });
+            }
+          }
+        );
+      } catch (error) {
+        res.json({
+          status: "error",
+          message: error,
+        });
+        return;
       }
-    );
-  } catch (error) {
-    res.json({
-      status: "error",
-      message: error,
-    });
-    return;
-  }
+    }
+  });
 }
 
 // ----------------------------
@@ -119,39 +135,53 @@ function createProduct(req: Request, res: Response) {
 // ----------------------------
 
 function updateProduct(req: Request, res: Response) {
-  try {
-    connection.execute(
-      "UPDATE products SET name = ?, description = ?, barcode = ?, image = ?, stock = ?, price = ?, category_id = ?, user_id = ?, status_id = ? WHERE id = ?",
-      [
-        req.body.name,
-        req.body.description,
-        req.body.barcode,
-        req.body.image,
-        req.body.stock,
-        req.body.price,
-        req.body.category_id,
-        req.body.user_id,
-        req.body.status_id,
-        req.params.id,
-      ],
-      function (error: any, results: any) {
-        if (error) {
-          res.json({
-            status: "error",
-            message: error,
-          });
-          return;
-        } else {
-          res.json({ status: "ok", message: "Product updated successfully" });
-        }
+  upload(req, res, async (error) => {
+    if (error instanceof multer.MulterError) {
+      res.json({
+        status: "error",
+        message: error,
+      });
+      return;
+    } else {
+      const image = req.file ? req.file.filename : null;
+      try {
+        connection.execute(
+          "UPDATE products SET name = ?, description = ?, barcode = ?, image = ?, stock = ?, price = ?, category_id = ?, user_id = ?, status_id = ? WHERE id = ?",
+          [
+            req.body.name,
+            req.body.description,
+            req.body.barcode,
+            image,
+            req.body.stock,
+            req.body.price,
+            req.body.category_id,
+            req.body.user_id,
+            req.body.status_id,
+            req.params.id,
+          ],
+          function (error: any, results: any) {
+            if (error) {
+              res.json({
+                status: "error",
+                message: error,
+              });
+              return;
+            } else {
+              res.json({
+                status: "ok",
+                message: "Product updated successfully",
+              });
+            }
+          }
+        );
+      } catch (error) {
+        res.json({
+          status: "error",
+          message: error,
+        });
       }
-    );
-  } catch (error) {
-    res.json({
-      status: "error",
-      message: error,
-    });
-  }
+    }
+  });
 }
 
 // ----------------------------
