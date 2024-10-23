83d0f91b4b440a62484a8e331790611ad2b5ad20
keekmx1 <kitthisack180897@gmail.com>
2024-10-23 11:12:43 +0700

    Create get product by id  function Success

--- routes/productRoute.ts
+++ routes/productRoute.ts
@@ -6,7 +6,7 @@ const router: Router = express.Router();
 
 // Define Router
 router.get("/", productController.getAllProducts);
-router.get("/id", productController.getAllProducts);
+router.get("/:id", productController.getAllProducts);
 
 // Export Router
 export default router;
