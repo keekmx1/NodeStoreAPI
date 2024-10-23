779797d182b81592d2ef10650da00a68dacd44dc
keekmx1 <kitthisack180897@gmail.com>
2024-10-23 13:50:43 +0700

    config multer

--- utils/multer_config.ts
+++ utils/multer_config.ts
@@ -1,4 +1,27 @@
-import { Request, Response } from "express";
-import multer, {FileFilterCallback} from "multer";
+import { Request } from "express";
+import multer, { FileFilterCallback } from "multer";
 import fs from "fs";
 
+const storage = multer.diskStorage({
+  destination(
+    req: Request,
+    file: Express.Multer.File,
+    callback: (error: Error | null, destination: string) => void
+  ) {
+    const path = "./uploads/images";
+    // Create directory if it doesn't exist
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
+    }
+    callback(null, path);
+  },
+
+  filename: (
+    req: Request,
+    file: Express.Multer.File,
+    callback: (error: Error | null, filename: string) => void
+  ) => {
+    const ext = file.mimetype.split("/")[1];
+    callback(null, `${file.fieldname}-${Date.now()}.${ext}`);
+  },
+});
