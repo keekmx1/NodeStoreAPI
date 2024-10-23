437679a62982e091a23de21762053e70d747e000
keekmx1 <kitthisack180897@gmail.com>
2024-10-23 15:00:02 +0700

    make directory upload

--- routes/productRoute.ts
+++ routes/productRoute.ts
@@ -7,9 +7,9 @@ const router: Router = express.Router();
 // Define Router
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getAllProducts);
-router.post("/create", productController.createProduct);
-router.put("/update/:id", productController.updateProduct);
-router.delete("/delete/:id", productController.deleteProduct);
+router.post("/", productController.createProduct);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 // Export Router
 export default router;
